ea320a725e417fed222ab8f323c034752e56d5d5
Ho Khoi <khoi.ho@access-company.com>
2023-09-17 16:55:33 +0900

    batch handle, multi mode

--- anime_download_service.py
+++ anime_download_service.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from urllib.parse import unquote
+from urllib.parse import unquote, urlparse, parse_qs
+from base64 import b32decode
 import deluge_client
 import feedparser
 import time
@@ -22,14 +23,14 @@ DOWNLOAD_FOLDER = "/media/pi/Drive/Anime"
 DRY_RUN = "dryrun" in os.environ
 DEBUG = "debug" in os.environ
 PATTERN = r"\[(.*?)\] (.*?) - (\d+(?:\.\d+)?)v?(\d*) \(1080p\) \[.*?\]\.mkv"
+BATCH_PATTERN = r"\[(.*?)\] (.*?) \((\d+(?:-\d+)?)\) \(1080p\) \[Batch\]"
 
 seen_entry_ids = set()
 anime_list = dict()
 
 ### Todo
-# deal with batch 
-# multiple mode( move only, delete, stop download in future, only batch)
 # deal with version
+
 
 def debug(*args, **kwargs):
     if DEBUG:
@@ -57,6 +58,7 @@ def check_for_push(pb_last_success):
             "%Y-%m-%d %H:%M:%S.%f"
         )
         debug(f"Find push after {t}")
+        print_ratelimit(headers)
 
     headers = {
         "Access-Token": API_KEY,
@@ -81,13 +83,31 @@ def check_for_push(pb_last_success):
     if response.status_code == 200:
         pushes = response.json()["pushes"]
         for push in pushes:
-            if "url" in push:
-                link = push["url"]
-                debug(f"Receive {link}")
-                download_anime(link)
+            push_handle(push)
         return current_time
     else:
         return pb_last_check
+
+
+def push_handle(push):
+    if "url" not in push:
+        return
+    link = push["url"]
+    debug(f"Receive {link}")
+    if "https://subsplease.org/shows" not in link:
+        return
+    if "body" in push:
+        if "batch" in push["body"]:
+            download_anime(link, batch=True)
+            return
+        if "move" in push["body"]:
+            download_anime(link, move=True)
+            return
+        if "stop" in push["body"]:
+            download_anime(link, stop=True)
+            return
+
+    download_anime(link)
 
 
 def check_rss(dry=False):
@@ -106,9 +126,9 @@ def check_rss(dry=False):
             link = entry.get("link", "")
             title, ep = get_title_ep(rss_title)
             info(f"RSS - Title: {title} - Episode: {ep}")
-            if title != None and title in anime_list:
+            if title != None and (title in anime_list):
                 push_notify(f"Download - Title: {title} - Episode: {ep}")
-                download_episode(title, link)
+                download_torrents(title, link)
 
 
 def get_title_ep(name):
@@ -119,11 +139,15 @@ def get_title_ep(name):
         return None, None
 
 
-def download_anime(url, batch=False):
-    global dirty
-    if "https://subsplease.org/shows" not in url:
-        return
+def magnet_to_hash(magnet):
+    query_params = parse_qs(urlparse(magnet).query)
+    if "xt" not in query_params:
+        return None
+    xt_value = query_params.get("xt", [])[0].split(":")[-1]
+    return bytes(b32decode(xt_value).hex(), encoding="ascii")
 
+
+def get_anime_info(url):
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
     with webdriver.Firefox(options=options) as driver:
@@ -142,49 +166,68 @@ def download_anime(url, batch=False):
         ]
         magnet_links = [i for i in magnet_links if "1080p" in unquote(i)]
         anime_title = driver.find_element(By.CLASS_NAME, "entry-title").text
+    return anime_title.replace("–",'-'), magnet_links
 
+
+def download_anime(url, batch=False, stop=False, move=False):
+    global dirty
+
+    debug(f"download_anime: {url=}, {batch=} {stop=} {move=}")
+    anime_title, magnet_links = get_anime_info(url)
     if anime_title in anime_list:
-        return  # or not? remove if in list?
+        if stop:
+            del anime_list[anime_title]
+            dirty = True
+        return
     anime_list[anime_title] = {"link": url}
     dirty = True
-    
-    push_notify(f"Download anime: {anime_title}")
+
+    if move:
+        push_notify(f"Move anime: {anime_title}")
+    else:
+        push_notify(f"Download anime: {anime_title}")
+
+    batch_link = []
+    episode_link = []
     for link in magnet_links:
-        _, ep = get_title_ep(unquote(link))
-        if batch:
-            
-            if ep != None:
-                info(f"Download {anime_title} Episode {ep}")
-                download_episode(anime_title, link)
-        elif batch:
-            if "[Batch]"
-            
+        if re.search(BATCH_PATTERN, unquote(link)):
+            batch_link.append(link)
+        elif get_title_ep(unquote(link))[1] != None:
+            episode_link.append(link)
+
+    if batch:
+        download_torrents(anime_title, batch_link, move)
+    else:
+        download_torrents(anime_title, episode_link, move)
 
 
-def download_episode(title, link):
+def download_torrents(title, links, move=False):
+    if DRY_RUN:
+        return
+
+    if isinstance(links, str):
+        links = [links]
 
     deluge = deluge_client.DelugeRPCClient("mypi", 58846, "kotori", "123456")
     deluge.connect()
+    torrents_dict = deluge.core.get_torrents_status({}, ["name"])
 
     download_path = os.path.join(DOWNLOAD_FOLDER, title)
     download_options = {
         "add_paused": False,
         "download_location": download_path,
     }
+    hash_list = [magnet_to_hash(i) for i in links]
 
-    if DRY_RUN:
-        return
-
-    try:
-        torrent_id = deluge.core.add_torrent_magnet(link, download_options)
-        torrent_exist = False
-    except Exception as e:
-        pattern = r"\b([0-9a-fA-F]{40})\b"
-        torrent_id = re.findall(pattern, str(e))[0]
-        torrent_exist = True
-    if torrent_exist:
-        deluge.core.move_storage([torrent_id], download_path)
-
+    deluge.core.move_storage(
+        [i for i in hash_list if i in torrents_dict], download_path
+    )
+    if not move:
+        for link in links:
+            if magnet_to_hash(link) not in torrents_dict:
+                _, ep = get_anime_info(link)
+                info(f"Download {title} Episode {ep}")
+                deluge.core.add_torrent_magnet(link, download_options)
     deluge.disconnect()
 
 
@@ -210,10 +253,10 @@ def write_list():
         yaml.dump(anime_list, anilist_file)
 
 
-rss_last_check = time.time()
-pb_last_check = time.time()
+rss_last_check = 0
+pb_last_check = 0
 pb_last_success = time.time()
-check_rss(True)
+# check_rss(True)
 
 config_folder = os.path.expanduser("~/.config/anime_download_service")
 os.makedirs(config_folder, exist_ok=True)
@@ -222,6 +265,11 @@ config_file_path = os.path.join(config_folder, "config.yaml")
 
 if os.path.exists(anilist_file_path):
     load_list()
+    tmp = anime_list
+    anime_list = dict()
+    for key, value in tmp.items():
+        new = key.replace("–",'-')
+        anime_list[new] = value
 else:
     write_list()
 
