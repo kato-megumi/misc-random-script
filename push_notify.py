f360c7a8845c0f271ce4edce72758d5454d64bc8
Ho Khoi <khoi.ho@access-company.com>
2023-09-17 18:51:37 +0900

    push notify

---
+++ push_notify.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3.9
+import os
+import yaml
+import requests
+from sys import argv
+from syslog import syslog
+
+PUSHBULLET_API_URL = "https://api.pushbullet.com/v2/pushes"
+
+config_folder = os.path.expanduser("~/.config/anime_download_service")
+config_file_path = os.path.join(config_folder, "config.yaml")
+
+if os.path.exists(config_file_path):
+    with open(config_file_path, "r") as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+        API_KEY = config["apikey"]
+else:
+    syslog("config.yaml not found")
+    exit()
+
+syslog('deluge test: the script started running')
+for arg in argv[1:]:
+    syslog(arg)
+
+
+push_data = {"type": "note", "title": "Anime Download Service", "body": f"Downloaded - {argv[1]}"}
+
+headers = {"Access-Token": API_KEY, "Content-Type": "application/json"}
+try:
+    requests.post(PUSHBULLET_API_URL, headers=headers, json=push_data)
+except Exception as e:
+    syslog("Send push notify fail.")
